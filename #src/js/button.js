1cd0d76a3e4f4bf6b7f98853396ec286abd14f94
DmitryT <dvtolmachev@omgtu.ru>
2022-04-05 12:02:05 +0600

    refersh1

--- #src/js/button.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-    $('button').on('click', function() {
-        $('.menu_mobile').removeClass('hidden').addClass('active');
-    });
-    
-    $('.close-menu').on('click', function() {
-        $('.menu_mobile').removeClass('active');
-    });
-});
